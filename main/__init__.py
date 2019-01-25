2826913c8f7c3df6a73bac49ad3ee1d5dbed0409
远哥制造 <root@yuangezhizao.cn>
2019-01-25 17:39:58 +0800

    jd_verify

--- main/__init__.py
+++ main/__init__.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, Flask!'
+
+@app.route('/jd_root.txt')
+def jd_verify():
+    return 'e95d2f4a675fe6f2b231093ef0892219c03e13e310499f23'
