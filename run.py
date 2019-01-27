d8115ae15bc460a9e0a0ba3645e1007e12bb596e
yuangezhizao <root@yuangezhizao.cn>
2019-01-27 11:33:58 +0800

    flask-script

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
-from main import app
+from main import create_app
+from flask_script import Manager
+
+# 创建app
+app = create_app()
+manager = Manager(app)
 
 if __name__ == '__main__':
-    app.run()
+    """
+    主函数启动http-server
+    """
+    manager.run()
