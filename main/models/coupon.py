d0deb90cf691f0c1f1ffdda0e59b1fb26779aff3
yuangezhizao <root@yuangezhizao.cn>
2019-01-28 14:32:40 +0800

    优惠券模型

---
+++ main/models/coupon.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env/ python3
+# -*- coding: utf-8 -*-
+"""
+    :Author: yuangezhizao
+    :Time: 2019/1/28 0028 13:20
+    :Site: http://www.yuangezhizao.cn
+    :Copyright: © 2019 yuangezhizao <root@yuangezhizao.cn>
+"""
+
+from main.plugins.extensions import db
+
+
+class Coupon(db.Document):
+    meta = {
+        'collection': 'projectj_coupons'
+    }
+    key = db.StringField(required=True, primary_key=True)  # 主键即为 _id，优惠券 key
+    name = db.StringField(required=True)  # 优惠券名称
+    strength = db.StringField(required=True)  # 优惠力度
