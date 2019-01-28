bf6ecd1e4cc7ae2c31405d43d17c4cffa932c5be
yuangezhizao <root@yuangezhizao.cn>
2019-01-28 21:33:06 +0800

    优惠券详情路由

--- main/apis/v0_1/coupon.py
+++ main/apis/v0_1/coupon.py
@@ -9,7 +9,7 @@
 from flask import request
 
 from main.apis.v0_1 import api_v0_1
-from main.apis.v0_1.outputs import success
+from main.apis.v0_1.outputs import success, bad_request
 from main.apis.v0_1.user import auth_required
 from main.models.coupon import Coupon
 
@@ -21,7 +21,29 @@ def coupon_index():
     paginated_coupons = Coupon.objects.paginate(page=page, per_page=10)
     r = []
     for coupon in paginated_coupons.items:
-        r.append(coupon)
+        new_coupon = {}
+        new_coupon['key'] = coupon['key']
+        new_coupon['name'] = coupon['name']
+        new_coupon['strength'] = coupon['strength']
+        r.append(new_coupon)
     next = page + 1 if paginated_coupons.has_next else page
     r = {'coupons': r, 'next': next, 'pages': paginated_coupons.pages, 'has_next': paginated_coupons.has_next}
+    return success(r)
+
+
+@api_v0_1.route('/coupon/detail', methods=['GET', 'POST'])
+@auth_required
+def coupon_detail():
+    # TODO：积分判断扣减
+    try:
+        key = request.get_json()['key']
+    except Exception as e:
+        print(e)
+        return bad_request('参数错误')
+    coupon = Coupon.objects(key=key).first()
+    r = {
+        'key': coupon.key,
+        'name': coupon.name,
+        'strength': coupon.strength,
+    }
     return success(r)
