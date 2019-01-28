8c6b12bcc251f20543993a7378308a8da8c047da
yuangezhizao <root@yuangezhizao.cn>
2019-01-28 14:35:00 +0800

    优惠券接口

---
+++ main/apis/v0_1/coupon.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env/ python3
+# -*- coding: utf-8 -*-
+"""
+    :Author: yuangezhizao
+    :Time: 2019/1/28 0028 13:34
+    :Site: http://www.yuangezhizao.cn
+    :Copyright: © 2019 yuangezhizao <root@yuangezhizao.cn>
+"""
+from flask import request
+
+from main.apis.v0_1 import api_v0_1
+from main.apis.v0_1.outputs import success
+from main.apis.v0_1.user import auth_required
+from main.models.coupon import Coupon
+
+
+@api_v0_1.route('/coupon')
+@auth_required
+def coupon_index():
+    page = request.get_json()['page'] if (request.get_json() is not None) else 1
+    paginated_coupons = Coupon.objects.paginate(page=page, per_page=10)
+    r = []
+    for coupon in paginated_coupons.items:
+        r.append(coupon)
+    return success({'coupons': r, 'pages': paginated_coupons.pages, 'has_next': paginated_coupons.has_next})
