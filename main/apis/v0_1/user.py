2eba200fa06e8d772f8411d101e8cfb5ebfc2576
yuangezhizao <root@yuangezhizao.cn>
2019-01-30 21:15:25 +0800

    Fix

--- main/apis/v0_1/user.py
+++ main/apis/v0_1/user.py
@@ -100,7 +100,7 @@ def user_login():
         signature = data['signature']
         encryptedData = data['encryptedData']
         iv = data['iv']
-        from_uid = data['formUID'] if ('formUID' in data) else -1
+        from_uid = data['formUID'] if (('formUID' in data) and isinstance(data['formUID'], int)) else -1
         print('wxcode：' + wxcode)
         print('userinfo：' + userinfo)
         print('signature：' + signature)
