1b1145656b175e93b600abf4a5fda67af404f80d
yuangezhizao <root@yuangezhizao.cn>
2019-01-30 10:57:53 +0800

    优惠券模型添加活动地址、优惠券地址

--- main/models/coupon.py
+++ main/models/coupon.py
@@ -16,5 +16,5 @@ class Coupon(db.Document):
     key = db.StringField(required=True, primary_key=True)  # 主键即为 _id，优惠券 key
     name = db.StringField(required=True)  # 优惠券名称
     strength = db.StringField(required=True)  # 优惠力度
-    # act_url = db.StringField(required=True)  # 活动地址
-    # url = db.StringField(required=True)  # 优惠券地址
+    act_url = db.StringField(required=True)  # 活动地址
+    url = db.StringField(required=True)  # 优惠券地址
