b2941b1b5f3e6d819ca1ef78012a7b42f45d2a5a
yuangezhizao <root@yuangezhizao.cn>
2019-02-17 20:04:59 +0800

    后台抽奖列表

---
+++ main/blueprints/web/lottery.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env/ python3
+# -*- coding: utf-8 -*-
+"""
+    :Author: yuangezhizao
+    :Time: 2019/2/17 0017 20:00
+    :Site: https://www.yuangezhizao.cn
+    :Copyright: © 2019 yuangezhizao <root@yuangezhizao.cn>
+"""
+import datetime
+
+from flask import request
+
+from main.apis.v0_1.outputs import success
+from main.models.lottery import Lottery
+from . import web_bp
+
+
+@web_bp.route('/lottery')
+def lottery_index():
+    page = int(request.args.get('page')) if (
+            (request.args.get('page') is not None) and (request.args.get('page') != '')) else 1
+    # TODO：wtforms
+    now_time = datetime.datetime.now()
+    paginated_lotteries = Lottery.objects(endTime__gt=now_time).order_by('endTime').paginate(page=page, per_page=10)
+    r = []
+    for lottery in paginated_lotteries.items:
+        new_lottery = {}
+        new_lottery['lotteryCode'] = lottery['lotteryCode']
+        new_lottery['lotteryName'] = lottery['lotteryName']
+        new_lottery['endTime'] = lottery['endTime'].strftime('%Y-%m-%d %H:%M:%S')
+        r.append(new_lottery)
+    next = page + 1 if paginated_lotteries.has_next else page
+    r = {'lotteries': r, 'next': next, 'pages': paginated_lotteries.pages, 'has_next': paginated_lotteries.has_next}
+    return success(r)
