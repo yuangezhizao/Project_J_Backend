35fe153f9f98ffecc2c1b68bc898adc347e64199
yuangezhizao <root@yuangezhizao.cn>
2019-02-28 19:51:24 +0800

    修复后台商品列表

--- main/blueprints/web/goods.py
+++ main/blueprints/web/goods.py
@@ -8,14 +8,12 @@
 """
 from flask import request
 
-from main.apis.v0_1 import api_v0_1
 from main.apis.v0_1.outputs import success
-from main.apis.v0_1.user import auth_required
 from main.models.good import Good
+from . import web_bp
 
 
-@api_v0_1.route('/good', methods=['GET', 'POST'])
-@auth_required
+@web_bp.route('/good', methods=['GET', 'POST'])
 def good_index():
     page = request.get_json()['page'] if ((request.get_json() is not None) and ('page' in request.get_json())) else 1
     paginated_goods = Good.objects.order_by('update_time').paginate(page=page, per_page=10)
@@ -24,8 +22,9 @@ def good_index():
         new_good = {}
         new_good['sku'] = good['sku']
         new_good['title'] = good['title']
-        new_good['prize'] = good['price_now']
-        new_good['update_time'] = good['update_time']
+        new_good['price_now'] = good['price_now']
+        new_good['update_time'] = good['update_time'].strftime('%Y-%m-%d %H:%M:%S')
+        new_good['url'] = good['url']
         r.append(new_good)
     next = page + 1 if paginated_goods.has_next else page
     r = {'goods': r, 'next': next, 'pages': paginated_goods.pages, 'has_next': paginated_goods.has_next}
