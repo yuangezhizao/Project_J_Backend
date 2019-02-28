c554f06a394f416f248adbe285692574942bced9
yuangezhizao <root@yuangezhizao.cn>
2019-02-28 19:28:20 +0800

    商品模型更新

--- main/models/good.py
+++ main/models/good.py
@@ -13,6 +13,20 @@ class Good(db.Document):
     meta = {
         'collection': 'projectj_goods'
     }
-    sku = db.StringField(required=True, primary_key=True)  # 主键即为 _id，商品 id
+    sku = db.IntField(required=True, primary_key=True)  # 主键即为 _id，商品 id
+    update_time = db.DateTimeField(required=True)  # 更新时间
+    price_now = db.FloatField(required=True)  # 当前价格
+    img = db.StringField(required=True)  # 图片地址
     title = db.StringField(required=True)  # 商品名称
-    prize = db.StringField(required=True)  # 商品价格
+    url = db.StringField(required=True)  # 商品地址
+    discountpercent = db.FloatField(required=True)  # 降价幅度
+    jd_price = db.FloatField(required=True)  # 京东价格
+    buy_count = db.IntField(required=True)  # 购买数量
+    his_price = db.FloatField(required=True)
+
+    cuxiao = db.StringField()  # 促销
+    coupon_discount = db.IntField()
+    coupon_quota = db.IntField()
+    coupon_roleid = db.StringField()
+    coupon_url = db.StringField()
+    coupon_key = db.StringField()
