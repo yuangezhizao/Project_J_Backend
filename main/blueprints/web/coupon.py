1bf879ce78fa07aad2a78d28eabb04aabb75b3ae
yuangezhizao <root@yuangezhizao.cn>
2019-03-03 18:28:17 +0800

    后台优惠券列表

--- main/blueprints/web/coupon.py
+++ main/blueprints/web/coupon.py
@@ -6,41 +6,48 @@
     :Site: https://www.yuangezhizao.cn
     :Copyright: © 2019 yuangezhizao <root@yuangezhizao.cn>
 """
-import math
+from flask import request
 
-import pymongo
-from flask import request, current_app
-
-from main.apis.v0_1.outputs import success
+from main.apis.v0_1.outputs import success, bad_request
+from main.models.coupon_orig import Coupon_Orig
 from . import web_bp
 
 
 @web_bp.route('/coupon', methods=['GET', 'POST'])
 def coupon_index():
-    conn = pymongo.MongoClient(current_app.config['MONGODB_SETTINGS']['host'])['jd']
     page = int(request.args.get('page')) if (
             (request.args.get('page') is not None) and (request.args.get('page') != '')) else 1
-    coupon_count = conn['coupon_detail'].count()
-    pages = int(math.ceil(float(coupon_count) / 10))
-    paginated_coupons = conn['coupon_detail'].find().sort('update_time', pymongo.DESCENDING).skip(page - 1).limit(10)
+    paginated_coupons = Coupon_Orig.objects(limitStr__exists=True).order_by('-update_time').paginate(page=page,
+                                                                                                     per_page=10)
     r = []
-    for coupon in paginated_coupons:
+    for coupon in paginated_coupons.items:
         new_coupon = {}
         new_coupon['key'] = coupon['key']
-        new_coupon['from_title'] = coupon['from_title'].strip()
+        new_coupon['roleid'] = coupon['roleid']
+        new_coupon['limitStr'] = coupon['limitStr']
+        new_coupon['quota'] = coupon['quota']
+        new_coupon['discount'] = coupon['discount']
+        new_coupon['discountpercent'] = coupon['discountpercent']
+        new_coupon['batchId'] = coupon['batchId']
+        new_coupon['batchCount'] = coupon['batchCount']
+        new_coupon['beginTime'] = coupon['beginTime'].strftime('%Y-%m-%d %H:%M:%S')
+        new_coupon['endTime'] = coupon['endTime'].strftime('%Y-%m-%d %H:%M:%S')
         new_coupon['update_time'] = coupon['update_time'].strftime('%Y-%m-%d %H:%M:%S')
-        new_coupon['from_url'] = coupon['from_url']
-
-        new_coupon['limitStr'] = coupon['limitStr'] if 'limitStr' in coupon else ''
-        new_coupon['quota'] = coupon['quota'] if 'quota' in coupon else ''
-        new_coupon['discount'] = coupon['discount'] if 'discount' in coupon else ''
-        new_coupon['batchCount'] = coupon['batchCount'] if 'batchCount' in coupon else ''
-        new_coupon['discountpercent'] = coupon['discountpercent'] if 'discountpercent' in coupon else ''
-        new_coupon['salesurl'] = coupon['salesurl'] if 'salesurl' in coupon else ''
-        new_coupon['url'] = coupon['url'] if 'url' in coupon else ''
-        new_coupon['batchurl'] = coupon['batchurl'] if 'batchurl' in coupon else ''
         r.append(new_coupon)
-    next = page + 1 if (10 * page < coupon_count) else page
-    has_next = True if (10 * page <= coupon_count) else False
-    r = {'goods': r, 'next': next, 'pages': pages, 'has_next': has_next}
+    next = page + 1 if paginated_coupons.has_next else page
+    r = {'coupons': r, 'next': next, 'pages': paginated_coupons.pages, 'has_next': paginated_coupons.has_next}
+    return success(r)
+
+
+@web_bp.route('/coupon/search', methods=['GET', 'POST'])
+def coupon_search():
+    # TODO：等学会 ES 的
+    try:
+        content = request.form['content']
+    except Exception as e:
+        print(e)
+        return bad_request('参数错误')
+    r = {
+        'content': content,
+    }
     return success(r)
