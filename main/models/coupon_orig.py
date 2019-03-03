59efe393caf130f401db4f2c6138e95aa0d41df9
yuangezhizao <root@yuangezhizao.cn>
2019-03-03 16:43:51 +0800

    后台优惠券预处理库

--- main/models/coupon_orig.py
+++ main/models/coupon_orig.py
@@ -9,14 +9,18 @@
 from main.plugins.extensions import db
 
 
-class Coupon(db.Document):
+class Coupon_Orig(db.Document):
     meta = {
         'collection': 'projectj_coupons'
     }
     key = db.StringField(required=True, primary_key=True)  # 主键即为 _id，优惠券 key
-    name = db.StringField(required=True)  # 优惠券名称
-    strength = db.StringField(required=True)  # 优惠力度
-    act_url = db.StringField(required=True)  # 活动地址
-    url = db.StringField(required=True)  # 优惠券地址
-    beginTime = db.DateTimeField(required=True)  # 开始时间
-    endTime = db.DateTimeField(required=True)  # 结束时间
+    endTime = db.DateTimeField()
+    limitStr = db.StringField()
+    discountpercent = db.FloatField()
+    roleid = db.StringField()
+    quota = db.StringField()
+    batchId = db.StringField()
+    discount = db.StringField()
+    beginTime = db.DateTimeField()
+    batchCount = db.IntField()
+    update_time = db.DateTimeField()
