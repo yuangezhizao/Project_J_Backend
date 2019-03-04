ee40736b971e7c7395364062dc1347f84a918a68
yuangezhizao <root@yuangezhizao.cn>
2019-03-04 11:22:23 +0800

    后台优惠券模型注释

--- main/models/coupon_orig.py
+++ main/models/coupon_orig.py
@@ -15,16 +15,16 @@ class Coupon_Orig(db.Document):
     }
     key = db.StringField(required=True, primary_key=True)  # 主键即为 _id，优惠券 key
     roleid = db.StringField()
-    limitStr = db.StringField()
-    quota = db.StringField()
-    discount = db.StringField()
-    discountpercent = db.FloatField()
+    limitStr = db.StringField()  # 仅可购买
+    quota = db.StringField()  # 满
+    discount = db.StringField()  # 减
+    discountpercent = db.FloatField()  # 折扣幅度
     batchId = db.StringField()
-    batchCount = db.IntField()
-    beginTime = db.DateTimeField()
-    endTime = db.DateTimeField()
+    batchCount = db.IntField()  # 限量
+    beginTime = db.DateTimeField()  # 开始时间
+    endTime = db.DateTimeField()  # 结束时间
     update_time = db.DateTimeField()
-    url = db.StringField()
-    salesurl = db.StringField()
-    batchurl = db.StringField()
-    from_url = db.StringField()
+    url = db.StringField()  # 优惠券地址
+    salesurl = db.StringField()  # 活动地址
+    batchurl = db.StringField()  # 可用地址
+    from_url = db.StringField()  # 来源地址
