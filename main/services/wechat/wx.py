946e774d2878ffbabf698c7ec75405783bb204d5
yuangezhizao <root@yuangezhizao.cn>
2019-04-08 10:34:16 +0800

    修复

--- main/services/wechat/wx.py
+++ main/services/wechat/wx.py
@@ -12,7 +12,7 @@ from wxpy import Bot
 
 
 def get_bot():
-    bot = Bot(cache_path=True, qr_path='/home/ubuntu/projectj_mp_backend/main/static/wx.png', console_qr=True)
+    bot = Bot(cache_path=True, qr_path='/home/ubuntu/projectj_mp_backend/main/static/wx.png', console_qr=False)
     bot.enable_puid()
     bot.messages.max_history = 0
     return bot
