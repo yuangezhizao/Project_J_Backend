edde5c4846c9446eaf966c1cec63e89cab2c3ac1
yuangezhizao <root@yuangezhizao.cn>
2019-05-01 12:46:10 +0800

    商品最大 100 页

--- main/apis/v0_1/good.py
+++ main/apis/v0_1/good.py
@@ -70,6 +70,7 @@ def good_search():
     # 枚举类型 按照什么排序规则排序
     count_allowed = [8, 28]
     page = int(request.args.get('page', 1))
+    page = page if page < 100 else 100
     s_content = request.args.get('content', '')
     s_type = int(request.args.get('type', 1))
     count = int(request.args.get('count', 28))
