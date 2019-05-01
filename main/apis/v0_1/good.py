5fd879bb0c87aed2f7ee6920f608f42a6c7817fa
yuangezhizao <root@yuangezhizao.cn>
2019-05-01 10:17:32 +0800

    商品搜索分页

--- main/apis/v0_1/good.py
+++ main/apis/v0_1/good.py
@@ -12,6 +12,7 @@ from main.apis.v0_1 import api_v0_1
 from main.apis.v0_1.outputs import success, bad_request, not_found
 from main.apis.v0_1.user import auth_required
 from main.models.good import Good
+from main.plugins.extensions import es
 
 
 @api_v0_1.route('/good', methods=['GET', 'POST'])
@@ -60,3 +61,58 @@ def good_detail():
         'coupon_url': good.coupon_url
     }
     return success(r)
+
+
+@api_v0_1.route('/good/search')
+def good_search():
+    Update_time, Disperent, Quota = 1, 2, 3
+    # 枚举类型 按照什么排序规则排序
+    count_allowed = [8, 28]
+    page = int(request.args.get('page', 1))
+    s_content = request.args.get('content', '')
+    s_type = int(request.args.get('type', 1))
+    count = int(request.args.get('count', 28))
+    if count not in count_allowed:
+        count = 28
+    query = {
+        'query': {
+            'multi_match': {
+                'query': s_content,
+                'fields': ['_id', 'title']
+                # 'fields': ['sku', 'title','jd_price','price_now']
+            }
+        },
+        'size': count,
+        'from': (page - 1) * count
+    }
+    if not s_content:
+        query = {
+            'size': count,
+            'from': (page - 1) * count
+        }
+    if Update_time == s_type:
+        sort = [{'update_time': 'desc'}]
+    elif Disperent == s_type:
+        sort = [{'discountpercent': 'asc'}]
+    # elif Quota == s_type:
+    #     sort = [{'quota': 'desc'}]
+    else:
+        sort = [{'update_time': 'desc'}]
+    query['sort'] = sort
+    r = es.search(index='jd', doc_type='projectj_goods', body=query)
+    data = []
+    for item in list(r['hits']['hits']):
+        data.append({
+            '_id': item.get('_id'),
+            # 'sort': item.get('sort'),
+            # '_type': item.get('_type'),
+            # '_source': {
+            'sku': item['_source'].get('sku'),
+            'title': item['_source'].get('title'),
+            'img': item['_source'].get('img'),
+            'jd_price': item['_source'].get('jd_price'),
+            'price_now': item['_source'].get('price_now'),
+        })
+    result = {  # 'total': r['hits']['hits']}
+        'result': data}
+    return success(result)
