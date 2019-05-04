0bfc0a25570459dca9ef96d4e1cd3dd7d36ff228
yuangezhizao <root@yuangezhizao.cn>
2019-05-04 12:24:20 +0800

    create_url 接口版

--- main/blueprints/root.py
+++ main/blueprints/root.py
@@ -6,9 +6,9 @@
     :Site: http://www.yuangezhizao.cn
     :Copyright: © 2019 yuangezhizao <root@yuangezhizao.cn>
 """
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, request
 
-from main.apis.v0_1.outputs import not_found
+from main.apis.v0_1.outputs import not_found, bad_request, success
 from main.models.short_url import Short_URL
 
 root_bp = Blueprint('root', __name__)
@@ -19,9 +19,22 @@ def hello_world():
     return render_template('index.html')
 
 
-@root_bp.route('/s/<id>')
-def short_url(id):
-    s = Short_URL.objescts(id=id).first()
-    if short_url is None:
+@root_bp.route('/create_url')
+def create_url():
+    from main.services.jd_union.open_api import create_url
+    common_url = request.args.get('common_url')
+    if not common_url:
+        return bad_request(data='参数不完整')
+    data = create_url(common_url)
+    if data[0]:
+        return success(data[1])
+    else:
+        return bad_request(data[1])
+
+
+@root_bp.route('/s/<jid>')
+def short_url(jid):
+    s = Short_URL.objects(jid=jid).first()
+    if s is None:
         return not_found('键无效')
     return redirect(s.url)
